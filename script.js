44235808226d6046ad8e0c1b3b35710c0ac256d7
Billy19092024 <billyjoshuaishimwe@gmail.com>
2025-09-04 19:52:14 +0200

    Add website files

---
+++ script.js
@@ -0,0 +1,23 @@
+function showDate() {
+  document.getElementById('demo').innerHTML = Date();
+}
+
+function myFunction() {
+  var elements = document.getElementsByClassName('cancer');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = 'none';
+  }
+}
+
+function light(state) {
+  var image = document.getElementById('myImage');
+  if (state == 1) {
+    image.src = 'image/pic_bulbon.gif';
+  } else {
+    image.src = 'image/pic_bulboff.gif';
+  }
+}
+
+window.onload = function() {
+  alert("Welcome to the Cancer Support Hub!");
+};
